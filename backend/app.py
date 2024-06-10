0c69e1c88f5220b4f21fa25d7baf0dded7047e73
Susan Zea <susanzea16@gmail.com>
2024-06-10 02:22:18 +0200

    added duration param to return days of week with their total workout time

--- backend/app.py
+++ backend/app.py
@@ -9,14 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://susanzea:EW1922!postgres@l
 db=SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-###### models
+###### models #####
 class WorkoutType(db.Model):
     __tablename__ = "workout_type"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    #workout type can be assigned to many sessions
     workout_sessions = db.relationship('WorkoutSession', backref='workout_type')
 
     def __repr__(self):
@@ -25,12 +24,6 @@ class WorkoutType(db.Model):
     def __init__(self, name):
         self.name = name
 
-def format_workout_type(workout_type):
-    return {
-        "id": workout_type.id,
-        "name": workout_type.name,
-        "created_at": workout_type.created_at,
-    }
 
 class WorkoutSession(db.Model):
     __tablename__ = "workout_session"
@@ -51,6 +44,14 @@ class WorkoutSession(db.Model):
         self.duration_min = duration_min
 
 
+##### formatters #####
+def format_workout_type(workout_type):
+    return {
+        "id": workout_type.id,
+        "name": workout_type.name,
+        "created_at": workout_type.created_at,
+    }
+
 
 def format_workout_session(workout_session):
     return {
@@ -62,53 +63,56 @@ def format_workout_session(workout_session):
     }
 
 
-
-
-
-
-
-
-##### routes
+##### routes #####
 # types
 @app.route("/workout-type", methods = ['POST'])
 def create_workout_type():
     name = request.json['name']
     workout_type = WorkoutType(name)
+
     db.session.add(workout_type)
     db.session.commit()
+
     return format_workout_type(workout_type)
+
 
 @app.route("/workout-type", methods = ["GET"])
 def get_workout_types():
     types = WorkoutType.query.order_by(WorkoutType.name.asc()).all()
     types_list = []
+
     for type in types:
         types_list.append(format_workout_type(type))
-    return {'workout_types': types_list}
+    
+    return {'workout_types': types_list, 'total': len(types_list)}
 
 
-
-#sessions
+# sessions
 @app.route("/workout-session", methods = ['POST'])
 def create_workout_session():
     workout_date = request.json['workout_date']
     workout_type_id = request.json['workout_type_id']
     duration_min = request.json['duration_min']
+
     workout_session = WorkoutSession(workout_date, workout_type_id, duration_min)
+    
     db.session.add(workout_session)
     db.session.commit()
+
     return format_workout_session(workout_session)
+
 
 @app.route("/workout-session", methods = ["GET"])
 def get_workout_sessions():
     week_start = request.args.get('week')
     type = request.args.get('type')
+    duration = request.args.get('duration')
 
     # if week param exists, filter for only sessions during that week
     query = WorkoutSession.query
+
     if (week_start):
-        # year, month, day = week.split('-')
-        week_end = datetime.strptime(week_start, '%Y-%m-%d') + timedelta(days=7)
+        week_end = datetime.strptime(week_start, '%Y-%m-%d') + timedelta(days=6)
         query = query.filter(WorkoutSession.workout_date.between(week_start, week_end))
     
     # if type param exists, filter for only sessions of that type
@@ -117,16 +121,27 @@ def get_workout_sessions():
         query = query.filter(WorkoutSession.workout_type_id == workout_type_id)
 
     sessions = query.order_by(WorkoutSession.workout_date.asc()).all()
-
     sessions_list = []
     for session in sessions:
         sessions_list.append(format_workout_session(session))
 
-    return {'workout_sessions': sessions_list, 'total': len(sessions_list)}
+    if (duration=='true'):
+        #create dictionary with date of selected week, set val to 0
+        workout_duration_by_weekday = {}
 
+        weekday_idx = 0
+        while weekday_idx <= 6:
+            date = datetime.strptime(week_start, '%Y-%m-%d') + timedelta(days=weekday_idx)
+            workout_duration_by_weekday[str(date)] = 0
+            weekday_idx += 1
 
+        #loop through sessions and add their duration to matching date in dictionary
+        for session in sessions:
+            workout_duration_by_weekday[str(session.workout_date)] = workout_duration_by_weekday[str(session.workout_date)] + session.duration_min
 
-
+        return workout_duration_by_weekday
+    else:
+        return {'workout_sessions': sessions_list, 'total': len(sessions_list)}
 
 
 if __name__ == "__main__":
