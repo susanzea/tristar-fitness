c103fb1e6b5a6b4220288590324a6fbe96a7e47b
Susan Zea <susanzea16@gmail.com>
2024-06-11 15:45:01 +0200

    form styled

--- frontend/src/components/SessionsIndex/Form.jsx
+++ frontend/src/components/SessionsIndex/Form.jsx
@@ -5,47 +5,53 @@ const Form = ({ workoutTypeOptions }) => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
 
-  console.log(watch("duration", "workout", "date")); // watch input value by passing the name of it
-
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <select
-        id='workout'
-        defaultValue={workoutTypeOptions[0].value}
-        {...register("workout", {
-          required: true,
-        })}>
-        {workoutTypeOptions.map((t, i) => {
-          return (
-            <option key={i} value={t.value}>
-              {t.label}
-            </option>
-          );
-        })}
-      </select>
+      <div className='form-section'>
+        <label htmlFor='workout'>Type</label>
+        <select
+          id='workout'
+          defaultValue={workoutTypeOptions[0].value}
+          {...register("workout", {
+            required: true,
+          })}>
+          {workoutTypeOptions.map((t, i) => {
+            return (
+              <option key={i} value={t.value}>
+                {t.label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
 
-      <label htmlFor='start'>Workout date:</label>
-      <input
-        label='date'
-        type='date'
-        id='start'
-        name='date'
-        defaultValue={new Date().toISOString().split("T")[0]}
-        min='2018-01-01'
-        max={new Date().toISOString().split("T")[0]}
-        {...register("date")}
-      />
+      <div className='form-section'>
+        <label htmlFor='date'>Date</label>
+        <input
+          label='date'
+          type='date'
+          id='workout-date'
+          name='date'
+          defaultValue={new Date().toISOString().split("T")[0]}
+          min='2018-01-01'
+          max={new Date().toISOString().split("T")[0]}
+          {...register("date")}
+        />
+      </div>
 
-      <input {...register("duration", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.duration && <span>This field is required</span>}
+      <div className='form-section'>
+        {" "}
+        <label htmlFor='duration'>Duration in min</label>
+        <input {...register("duration", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.duration && <span>This field is required</span>}
+      </div>
 
-      <input type='submit' />
+      <input id='submit' type='submit' />
     </form>
   );
 };
